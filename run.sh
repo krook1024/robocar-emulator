b0a067c7e457eb9ddf6deaa838b89c0fdd35b8fc
krook1024 <krook1024@gmail.com>
2019-10-14 11:20:01 +0200

    progress

--- run.sh
+++ run.sh
@@ -2,19 +2,20 @@
 # runs the OOCWC project.
 
 # run RCEMU
-lxterminal --command="/bin/bash -c 'cd /root/Desktop/justine/rcemu; src/smartcity --osm=../debrecen.osm --city=Debrecen --shm=JustineSharedMemory --node2gps=../../lmap.txt'"
+lxterminal --command="/bin/bash -c 'cd /root/Desktop/justine/rcemu; src/smartcity --node2gps=../../lmap.txt'"
 
 sleep 10
 
-# run RCLOG
-lxterminal --command="/bin/bash -c 'cd /root/Desktop/justine/rclog; ../rcemu/src/traffic 10007|tee traffic.log|java -jar target/site/justine-car-window-0.0.10-jar-with-dependencies.jar ../../lmap.txt'"
+# run traffic server + Rclog
+lxterminal --command="/bin/bash -c 'cd /root/Desktop/justine/rcemu; src/traffic'"
+lxterminal --command="/bin/bash -c 'cd /root/Desktop/justine/rclog; java -jar target/site/justine-car-window-0.0.10-jar-with-dependencies.jar ../../lmap.txt'"
 
 sleep 10
 
 # run sampleSHMclient
-lxterminal --command="/bin/bash -c 'cd /root/Desktop/justine/rcemu; src/sampleshmclient '"
+#lxterminal --command="/bin/bash -c 'cd /root/Desktop/justine/rcemu; src/sampleshmclient '"
 
-sleep 10
+#sleep 10
 
 # telnet
-lxterminal --command="/bin/bash -c 'cd /root ; (sleep 1; echo \"<init Norbi 100>\"; sleep 1;) | telnet localhost 10007'"
+#lxterminal --command="/bin/bash -c 'cd /root ; (sleep 1; echo \"<init Norbi 100>\"; sleep 1;) | telnet localhost 10007'"
